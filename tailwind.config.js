1563862c43a0af7472809998d99f62a381461cf9
Abubakar-Aijaz <abubakar123456654123@gmail.com>
2024-07-04 17:04:51 +0500

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   content: [
     './dist/**/*.html', // Include HTML files in the dist folder and its subdirectories
-    './dist/**/*.js',   // Include JS files in the dist folder and its subdirectories
-    './src/**/*.js',    // Include JS files in the src folder and its subdirectories
+    './dist/**/*.js'   // Include JS files in the dist folder and its subdirectories
   ],
   theme: {
     extend: {
